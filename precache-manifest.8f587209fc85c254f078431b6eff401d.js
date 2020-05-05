608ed367dc6d7b0ac2d30545a4d571b7eb37150d
Daniel Li <danielli12750@gmail.com>
2020-05-05 20:01:08 +1000

    Updates

---
+++ precache-manifest.8f587209fc85c254f078431b6eff401d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f4b888cc33446beb9f909cb83e7e63dd",
+    "url": "/quote-machine/index.html"
+  },
+  {
+    "revision": "33ef0f0724ecabd661af",
+    "url": "/quote-machine/static/css/main.7e0e7f41.chunk.css"
+  },
+  {
+    "revision": "4a4b5de48db5a6c28648",
+    "url": "/quote-machine/static/js/2.9e3d3789.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/quote-machine/static/js/2.9e3d3789.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "33ef0f0724ecabd661af",
+    "url": "/quote-machine/static/js/main.323a03e5.chunk.js"
+  },
+  {
+    "revision": "aae0ce78e2995d5dba4d",
+    "url": "/quote-machine/static/js/runtime-main.e5ed0aff.js"
+  }
+]);
